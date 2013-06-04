9f5bdb1744d628767a6c99b603269293fbb6f1c2
Mark Willis <mark.w@immat.co.uk>
2013-06-04 22:48:13 +0100

    initial dnode in

--- dnodeClient.js
+++ dnodeClient.js
@@ -1,9 +1,27 @@
 var dnode = require('dnode');
 
-var d = dnode.connect(5004);
+var d = dnode.connect(5004); // connect
+
+// my game board - for a start just use a 4x4 grid
+
+var board = [
+	[0 , 0 , 0, 1],
+	[0 , 0 , 0, 0],
+	[0 , 0 , 0, 0],
+	[0 , 0 , 0, 0]
+];
+
+
 d.on('remote', function (remote) {
-    remote.startGame('user1', function (s) {
-        console.log('beep => ' + s);
+	var regData = {
+		username: process.argv.pop(),
+		board: board
+	};
+
+	console.log('user: ', regData.username);
+
+    remote.startGame(regData, function (s) {
+        console.log('Waiting for game start => ', s);
         d.end();
     });
 });
