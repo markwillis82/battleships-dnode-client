a719cef7ffdb88668991e85acaa7261b147f4d3f
Mark Willis <mark.w@immat.co.uk>
2013-06-06 13:12:38 +0100

    working demo board

--- scripts/charmBoard.js
+++ scripts/charmBoard.js
@@ -2,32 +2,36 @@ var charm = require('charm')();
 charm.pipe(process.stdout);
 charm.reset();
 
-var colors = [ 'red', 'cyan', 'yellow', 'green', 'blue' ];
-var text = 'Always after me lucky charms.';
+charm.cursor(false);
 
 var offset = 0;
 var gridSize = 10;
+var redraw = 0;
 
-var y = 2;
-charm.foreground('red').write('\n--------------------------------');
-// charm.move(0, 0);
+setInterval(function() {
+    charm.foreground('red').write('\n--------------------------------');
+    // charm.move(0, 0);
 
-for(var x = 0; x <= gridSize; x++) {
-    charm.position(0, x+2);
-    charm.write('\n|');
-        // .foreground('white')
-        // .write('. . . . . . . . . . .')
-        // .foreground('red')
-        // .write('|');
-    for (var y = 0; y < 10; y++) {
-        charm.foreground('white').write(' . ');
+    for(var x = 0; x <= gridSize; x++) {
+        charm.position(0, x+2);
+        charm.write('\n|');
+            // .foreground('white')
+            // .write('. . . . . . . . . . .')
+            // .foreground('red')
+            // .write('|');
+        for (var y = 0; y < gridSize; y++) {
+            charm.foreground('white').write(' . ');
+        }
+        charm.foreground('red').write('|');
     }
-    charm.foreground('red').write('|');
-}
-// charm.position(0,gridSize+1);
-charm.foreground('red').write('\n--------------------------------');
-
+    // charm.position(0,gridSize+1);
+    charm.foreground('red').write('\n--------------------------------');
+    charm.foreground('green').write('\nredraw: '+ redraw);
+    redraw++;
+    charm.position(0,0);
 charm.write('\n'); // end
+},1000);
+
 // charm.position(0, gridSize+2);
 
 
