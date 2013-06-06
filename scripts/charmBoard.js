9031397a2c88e3b44e86865a887fb369668628b7
Mark Willis <mark.w@immat.co.uk>
2013-06-06 13:18:32 +0100

    now we draw a board from an array

--- scripts/charmBoard.js
+++ scripts/charmBoard.js
@@ -1,7 +1,20 @@
+var testBoard = [
+    [ 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ],
+    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ],
+    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+    [ 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ],
+    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+    [ 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1 ],
+    [ 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+    [ 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ],
+    [ 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
+];
+
+
 var charm = require('charm')();
 charm.pipe(process.stdout);
 charm.reset();
-
 charm.cursor(false);
 
 var offset = 0;
@@ -9,23 +22,20 @@ var gridSize = 10;
 var redraw = 0;
 
 setInterval(function() {
-    charm.foreground('red').write('\n--------------------------------');
-    // charm.move(0, 0);
+    charm.foreground('red').write('\n--------------------------------'); // draw top
 
-    for(var x = 0; x <= gridSize; x++) {
-        charm.position(0, x+2);
-        charm.write('\n|');
-            // .foreground('white')
-            // .write('. . . . . . . . . . .')
-            // .foreground('red')
-            // .write('|');
+    for(var x = 0; x < gridSize; x++) {
+        charm.position(0, x+2); // get new row
+        charm.write('\n|'); // draw left wall
+
         for (var y = 0; y < gridSize; y++) {
-            charm.foreground('white').write(' . ');
+            charm.foreground('white').write(/*' . '*/ ' ' + (testBoard[x][y] ? testBoard[x][y] : '.') + ' '); // draw result
         }
-        charm.foreground('red').write('|');
+
+        charm.foreground('red').write('|'); // draw right wall
     }
-    // charm.position(0,gridSize+1);
-    charm.foreground('red').write('\n--------------------------------');
+
+    charm.foreground('red').write('\n--------------------------------'); // draw bottom
     charm.foreground('green').write('\nredraw: '+ redraw);
     redraw++;
     charm.position(0,0);
